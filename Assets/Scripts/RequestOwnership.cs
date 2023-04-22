932a1bba79879f1ff38eb734129f32ba43568cb0
cyrilmedab <cyrildmed@gmail.com>
2023-04-22 12:02:08 -0400

    Early stage multiplayer and MR scene created wit grabbables

---
+++ Assets/Scripts/RequestOwnership.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Normal.Realtime;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(RealtimeView), typeof(RealtimeTransform))]
+public class RequestOwnership : MonoBehaviour
+{
+    private RealtimeView _realtimeView;
+    private RealtimeTransform _realtimeTransform;
+
+    private void Start()
+    {
+        _realtimeView = GetComponent<RealtimeView>();
+        _realtimeTransform = GetComponent<RealtimeTransform>();
+    }
+
+    public void RequestObjectOwnership(SelectEnterEventArgs args)
+    {
+        _realtimeView.RequestOwnership();
+        _realtimeTransform.RequestOwnership();
+    }
+}
